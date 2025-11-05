8d57e7655d69a021254e71cdfd5b4bc5268e9440
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-11-05 09:05:02 +0900

    refactor: remove unused useRef import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { HeaderContainer } from "./components/header/HeaderContainer";
 import { InputFormContainer } from "./components/input-form/InputFormContainer";
